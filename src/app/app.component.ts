f2cfbd4a271b4fdf38d594c1fba624807a0e30b8
codewonder-dev <codewonder.dev@gmail.com>
2025-02-17 01:38:49 +0100

    feat: implement countdown feature using signals and effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, effect, EffectCleanupRegisterFn, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-signals';
+  title = signal('angular-signals');
+  #countdown = signal(0);
+
+  #titleEffect = effect(() => {
+    if (this.#countdown() === 10) {
+      this.title.set("Blast off 🚀🚀🚀");
+      return;
+    }
+    this.title.set("Countdown: " + this.#countdown());
+  });
+
+  #countdownEffect = effect((onCleanup: EffectCleanupRegisterFn) => {
+    const timers: any[] = [];
+    for (let i = 0; i <= 10; i++) {
+      const timer = setTimeout(() => {
+        this.#countdown.set(i);
+      }, 100 * i)
+
+      timers.push(timer);
+    }
+
+    onCleanup(() => {
+      timers.forEach(clearTimeout);
+    })
+  });
 }
